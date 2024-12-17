7b578caa059a8594dbf75e181ddbd57eb317413b
hussain-si <hussain.psum@gmail.com>
2024-12-17 08:32:38 +0530

    Added Basic

--- src/main/java/Demo.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.Test;
-
-public class Demo {
-
-   @Test
-    public  void display() {
-
-    }
-}
---
+++ src/test/java/HandleBasicHTMLControls.java
@@ -0,0 +1,68 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleBasicHTMLControls {
+    public static void main(String[] args) throws Exception {
+        // Setup WebDriver using WebDriverManager
+
+
+        // Launch Chrome browser
+
+
+        // Maximize the browser window
+
+
+        // Navigate to the target URL
+
+
+        // Pause for 3 seconds to allow the page to load (not recommended in real-time projects, use WebDriverWait)
+
+
+        // Select the "Female" radio button using its ID
+
+
+        // Pause for 3 seconds to observe the action
+
+
+        // Select the "English" checkbox using its ID
+
+
+        // Pause for 3 seconds to observe the action
+
+
+        // Locate the "Hindi" checkbox using its ID
+
+
+        // Check the "Hindi" checkbox
+
+
+        // Pause for 3 seconds to observe the action
+
+
+        // Verify if the "Hindi" checkbox is selected
+
+            // Uncheck the "Hindi" checkbox if it is selected
+
+
+        // Pause for 3 seconds to observe the action
+
+
+        // Click on the "Register" button using its ID
+
+
+        // Retrieve and print the message displayed after clicking the "Register" button
+
+
+        // Pause for 3 seconds to observe the output
+
+
+        // Click on the link to navigate to the home page
+
+
+        // Close the browser session (optional but recommended in practice)
+
+    }
+}
